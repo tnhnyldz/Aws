2a69f1d5b6b88680295d25ab561e888018814dc4
Tnhnyildiz <tunahanyildiz@outlook.it>
2024-11-22 19:57:26 +0300

    Add project files.

---
+++ SQSConsumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+
+/*
+for server or computers that not have aws cli
+
+var awsAccesKeyId = "";
+var awsSecretAccesKey = "";
+var awsRegion = Amazon.RegionEndpoint.EUCentral1;
+var sqsClient = new AmazonSQSClient(awsAccesKeyId, awsAccesKeyId, awsRegion);
+*/
+
+var awsRegion = Amazon.RegionEndpoint.EUCentral1;
+var sqsClient = new AmazonSQSClient(awsRegion);
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+	QueueUrl = queueUrlResponse.QueueUrl,
+	AttributeNames = new List<string> { "All" },
+	MessageAttributeNames = new List<string> { "All" }
+};
+var cts = new CancellationTokenSource();
+
+while (!cts.IsCancellationRequested)
+{
+	var response = await sqsClient.ReceiveMessageAsync(receiveMessageRequest);
+
+	foreach (var message in response.Messages)
+	{
+		//send sms or send email or do something
+
+		Console.WriteLine("----------------------------------------");
+		Console.WriteLine($"MessageId:{message.MessageId}");
+		Console.WriteLine($"MessageBody{message.Body}");
+
+		//await sqsClient.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle);
+	}
+
+	await Task.Delay(100, cts.Token);
+}
---
+++ SQSPublisher/Program.cs
@@ -0,0 +1,49 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using System.Text.Json;
+
+
+/*
+for server or computers that not have aws cli
+
+var awsAccesKeyId = "";
+var awsSecretAccesKey = "";
+var awsRegion = Amazon.RegionEndpoint.EUCentral1;
+var sqsClient = new AmazonSQSClient(awsAccesKeyId, awsAccesKeyId, awsRegion);
+*/
+
+var awsRegion = Amazon.RegionEndpoint.EUCentral1;
+var sqsClient = new AmazonSQSClient(awsRegion);
+
+var customer = new
+{
+	FirstName = "Teoman",
+	LastName = "Yildiz",
+	Age = 22
+};
+
+//if you are authenticated you can get your url with this method
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+	QueueUrl = queueUrlResponse.QueueUrl,
+	MessageBody = JsonSerializer.Serialize(customer),
+	MessageAttributes = new Dictionary<string, MessageAttributeValue>
+	{ 
+		{
+			"MessageType",
+			new MessageAttributeValue
+			{
+				DataType = "String",
+				StringValue = "Customer"
+			}
+		}
+	}
+};
+
+
+
+var response = sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.ReadLine();
